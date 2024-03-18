bef18dff7a73e37b692f90dc223ac06ad8b6bac6
lizeth.aragon <lizeth.aragon@sophossolutions.com>
2024-03-18 09:19:51 -0500

    Configuración de rutas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { CartPage } from "../pages/CartPage";
+import { CategoryPage } from "../pages/CategoryPage";
+import { HomePage } from "../pages/HomePage";
+import { ProductDetailPage } from "../pages/ProductDetailPage";
 
 export const AppRouter = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/category" element={<CategoryPage />} />
+      <Route path="/product/:productId" element={<ProductDetailPage />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
